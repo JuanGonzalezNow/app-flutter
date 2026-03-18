2fb90724cb7ebc0646486e9b26b8c6cdb2a87263
Alexis-QvR <vanegasruizq@gmail.com>
2026-03-18 17:24:23 -0500

    app_clima 2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('noy me la pela omg'))),
+      home: Scaffold(body: Center(child: Text('noy y keren mis papis'))),
     );
   }
 }
