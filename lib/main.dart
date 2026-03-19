8ca45e605746558efe2d09b8de2ba066b766e253
NoNoy-25 <andresnoy78@gmail.com>
2026-03-18 21:19:10 -0500

    Quedin daño todo

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('noy y keren mis papis'))),
+      home: Scaffold(body: Center(child: Text('Juan y Angie x siempre'))),
     );
   }
 }
